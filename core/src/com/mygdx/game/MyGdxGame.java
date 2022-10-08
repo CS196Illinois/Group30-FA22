0b6d0e9fe32ad1f33be1093d791b2b0574bf09b5
jtw8 <113747696+jtw8@users.noreply.github.com>
2022-10-08 11:55:12 -0500

    Writing Box2D Code (Ball and Platform)

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,31 +1,72 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.*;
+import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	World world;
+	Box2DDebugRenderer debugRenderer;
+	OrthographicCamera camera;
+	BodyDef groundBodyDef;
+	Body groundBody;
+	PolygonShape groundBox;
+	BodyDef bodyDef;
+	Body body;
+	CircleShape circle;
+	FixtureDef fixtureDef;
+	Fixture fixture;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		Box2D.init();
+		world = new World(new Vector2(0, -10), true);
+		debugRenderer = new Box2DDebugRenderer();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+		groundBodyDef = new BodyDef();
+		groundBodyDef.position.set(new Vector2(0, 10));
+		groundBody = world.createBody(groundBodyDef);
+		groundBox = new PolygonShape();
+		groundBox.setAsBox(camera.viewportWidth, 10.0f);
+		groundBody.createFixture(groundBox, 0.0f);
+		bodyDef = new BodyDef();
+		bodyDef.type = BodyType.DynamicBody;
+		bodyDef.position.set(300, 400);
+		body = world.createBody(bodyDef);
+		circle = new CircleShape();
+		circle.setRadius(6f);
+		fixtureDef = new FixtureDef();
+		fixtureDef.shape = circle;
+		fixtureDef.density = 0.5f;
+		fixtureDef.friction = 0.4f;
+		fixtureDef.restitution = 0.6f;
+		fixture = body.createFixture(fixtureDef);
 	}
+
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		debugRenderer.render(world, camera.combined);
+		world.step(1/10f, 6, 2);
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		world.dispose();
+		groundBox.dispose();
+		circle.dispose();
 	}
 }
