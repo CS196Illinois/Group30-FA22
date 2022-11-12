45ec8b21fcce21432ff21bbc761f52bf4bf7bbfb
jtw8 <jtw8@illinois.edu>
2022-11-12 12:44:10 -0600

    Limiting Upward Movement.

--- core/src/com/mygdx/game/bodyWorks/bodyPlayer/BodyPlayer.java
+++ core/src/com/mygdx/game/bodyWorks/bodyPlayer/BodyPlayer.java
@@ -9,21 +9,49 @@ import com.mygdx.game.bodyWorks.BodyWorks;
 public class BodyPlayer extends BodyWorks {
 
 
+    private boolean upMovementLock;
+    private boolean upMovementUnlock;
+    int waitInt;
+
     public BodyPlayer() {
-        super(BodyDef.BodyType.DynamicBody,new Vector2(100, 200), 1/2, 0.15f,0.1f, 0.6f);
+        super(BodyDef.BodyType.DynamicBody,new Vector2(100, 200), 1/2, 0.05f,0.1f, 0.6f);
+        upMovementLock = true;
+        waitInt = 100;
     }
 
     public void inputWorks(){
-            if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-                super.x = -1;
-            } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-                super.x = 1;
-            } else {
-                super.x = 0;
-            }
-            super.y = 0;
+        System.out.println(waitInt);
+        if (waitInt != 0 && waitInt != 100){
+            waitInt--;}
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            super.x = -1;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            super.x = 1;
+        } else {
+            super.x = 0;
+        }
+        super.y = 0;
         if(Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            super.y = 1;
+            System.out.println(upMovementLock);
+            if(super.body.getLinearVelocity().y > 15 && !upMovementUnlock){
+                waitInt--;
+                upMovementLock = true;
+            }
+            if (super.body.getLinearVelocity().y < 0) {
+                upMovementUnlock = true;
+
+            }
+            if(upMovementUnlock && Math.abs(super.body.getLinearVelocity().y) < 1 && waitInt == 100) {
+                waitInt--;
+                upMovementLock = false;
+                upMovementUnlock = false;
+            }
+            if(!upMovementLock) {
+                super.y = 1;
+            }
+            if (waitInt == 0) {
+                waitInt = 100;
+            }
         }
 
 
